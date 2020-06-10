702dff7936e08097001eded2a582d9487b20e089
agniadvani <advani.agni.agni@gmail.com>
2020-06-11 03:32:19 +0530

    displaying pictures

---
+++ 02_displaying_pictures/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+func main() {
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/view", view)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	xs := strings.Split(c.Value, "|")[1:]
+	tpl.ExecuteTemplate(w, "index.html", xs)
+}
+
+func upload(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	if req.Method == http.MethodPost {
+		mf, fh, err := req.FormFile("upimg")
+		check(err)
+		defer mf.Close()
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+		wd, err := os.Getwd()
+		check(err)
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		check(err)
+		defer nf.Close()
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+		appendValues(w, c, fname)
+	}
+	tpl.ExecuteTemplate(w, "upload.html", nil)
+}
+func view(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	xs := strings.Split(c.Value, "|")[1:]
+	tpl.ExecuteTemplate(w, "view.html", xs)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+func appendValues(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
